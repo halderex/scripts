079df665ff38a1a59eeecc7e368f93f132af253b
Henrik Aldermo <66224013+halderex@users.noreply.github.com>
2022-03-28 12:08:20 +0200

    Update Onboard.ps1

--- Onboard.ps1
+++ Onboard.ps1
@@ -1,6 +1,8 @@
+Write-Output "Importing Onboarding Script"
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Script -name Get-WindowsAutopilotInfo -Force
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
+Write-Output "Starting Onboarding Script. Sign in when prompted."
 Set-Location $env:ProgramFiles\WindowsPowerShell\Scripts\
 .\Get-WindowsAutoPilotInfo -Online
