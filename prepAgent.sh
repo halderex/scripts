510528a78a521a0a3cbc6d6ac38ae338811c204d
Henrik Aldermo <henrik.aldermo@griffeye.com>
2023-09-21 22:34:21 +0200

    Create subdir

--- prepAgent.sh
+++ prepAgent.sh
@@ -10,6 +10,8 @@ setfacl -Rb /home/AzDevOps
 sudo su -c "echo 'AzDevOps  ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/01_AzDevOps && chmod 0440 /etc/sudoers.d/01_AzDevOps"
 
 sudo mkdir /agent && chmod 775 /agent
-sudo ln -s /datadisks/disk1/_work /agent/_work
 sudo chown -R AzDevOps:AzDevOps /agent
+
+sudo mkdir /datadisks/disk1/_work
+sudo ln -s /datadisks/disk1/_work /agent/_work
 sudo chown -R AzDevOps:AzDevOps /datadisks/disk1
