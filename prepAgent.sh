88353555b92c5c7d3b387244d94562a614a197ba
Henrik Aldermo <henrik.aldermo@griffeye.com>
2023-09-21 22:03:27 +0200

    Remove symlink

--- prepAgent.sh
+++ prepAgent.sh
@@ -9,6 +9,4 @@ setfacl -Rb /home/AzDevOps
 
 sudo su -c "echo 'AzDevOps  ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/01_AzDevOps && chmod 0440 /etc/sudoers.d/01_AzDevOps"
 
-sudo ln -s /datadisks/disk1 /agent
-sudo chmod 775 /datadisks/disk1
-sudo chown -R AzDevOps:AzDevOps /datadisks/disk1
+
